044829691501b3fed6d29e9449be848b0fba5f95
Jevin Sweval <jevinsweval@gmail.com>
2023-08-04 13:56:44 -0400

    wip

--- find-elfs-with-cfg-calls.py
+++ find-elfs-with-cfg-calls.py
@@ -1,40 +1,11 @@
 #!/usr/bin/env python3
 
-# import sys
-
-# from path import Path
-
-# cfg_syms = open(sys.argv[3], "rb").read().splitlines()
-# # print(cfg_syms)
-
-# cfg_elfs = set()
-# for filename in open(sys.argv[1]).read().splitlines():
-#     fullpath = Path(sys.argv[2]) / Path(filename)
-#     with open(fullpath, "rb") as f:
-#         buf = f.read()
-#         for sym in cfg_syms:
-#             # if "libccl_cfg_ini.so" in str(fullpath):
-#             #     print("found cfg lib itself")
-#             #     if sym in buf:
-#             #         print(f"found {sym}")
-#             #     else:
-#             #         print(f"no sym {sym}")
-#             if sym in buf:
-#                 cfg_elfs.add(fullpath)
-
-# for elf in cfg_elfs:
-#     print(elf)
-
-#!/usr/bin/env python3
-
-import argparse
 import io
 import sys
 from typing import Optional
 
 import lief
-import wrapt
-from attrs import define, field
+from attrs import define
 from cxxfilt import demangle
 from path import Path
 from rich import inspect, print
@@ -43,6 +14,24 @@ from typing_extensions import Self
 
 ELF_MAGIC = b"\x7FELF"
 CFG_ELF = Path("libccl_cfg_ini.so")
+
+
+class TextOutputPath(Path):
+    def __init__(self: Self, path: Path) -> None:
+        super().__init__(path)
+
+    @property
+    def file(self: Self) -> io.TextIOWrapper:
+        if not hasattr(self, "_file"):
+            if self == "-":
+                self._file: io.TextIOWrapper = sys.stdout
+            else:
+                self._file: io.TextIOWrapper = open(self, "w")
+        return self._file
+
+    @classmethod
+    def to_text_output_path(cls: type(Self), path: Path) -> Self:
+        return cls(path)
 
 
 def get_cfg_exports(cfg_elf: Path) -> list[str]:
@@ -55,8 +44,8 @@ def get_cfg_exports(cfg_elf: Path) -> list[str]:
 def elf_imported_cfg_exports(elf_path: Path, cfg_exports_set: set[str]) -> set[str]:
     b = lief.parse(elf_path)
     if not isinstance(b, lief.ELF.Binary):
-        raise TypeError("f{elf_path.name()} is not a lief.ELF.Binary")
-    imp_syms = {s.name for s in b.imported_symbols}
+        raise TypeError(f"{elf_path.name()} is not a lief.ELF.Binary")
+    imp_syms = {s.name for s in b.symbols}
     return imp_syms.intersection(cfg_exports_set)
 
 
@@ -67,7 +56,15 @@ class CfgUsingElf:
     cfg_sym_imports_demangled: list[str]
 
 
-def real_main(args):
+def pretty_print_cfg_using_elfs(
+    cfg_using_elfs: list[CfgUsingElf], cfg_exports: list[str], out_path: TextOutputPath
+) -> None:
+    info = {}
+    info["num_cfg_using_elfs"] = len(cfg_using_elfs)
+    sym_used_n_times = [0] * len(cfg_exports)
+
+
+def real_main(args) -> None:
     cfg_using_elfs: list[CfgUsingElf] = []
     cfg_elf: Optional[Path] = None
     for f in args.in_path.walkfiles():
@@ -77,7 +74,7 @@ def real_main(args):
                 break
     if cfg_elf is None:
         raise LookupError("Couldn't find {CFG_ELF}")
-    cfg_exports: list[str] = get_cfg_exports(f)
+    cfg_exports: list[str] = get_cfg_exports(cfg_elf)
     cfg_exports_demangled: list[str] = sorted([demangle(s) for s in cfg_exports])
     cfg_exports_set: set[str] = set(cfg_exports)
     cfg_exports_set_demangled: set[str] = set(cfg_exports_demangled)
@@ -109,36 +106,6 @@ def real_main(args):
     print(cfg_using_elfs)
 
 
-# class OpenedTextPath(io.TextIOWrapper):
-#     def __init__(self, p: Path, mode: str = "r"):
-#         # super
-#         if mode not in ("r", "w"):
-#             raise TypeError("mode must be \"r\" or \"w\"")
-#         if p == "-":
-#             if mode != "w":
-#                 raise TypeError("mode for \"-\" (stdout) must be \"w\"")
-#             return sys.stdout
-#         return open(p, mode)
-
-
-class TextOutputPath(Path):
-    def __init__(self: Self, path: Path) -> None:
-        super().__init__(path)
-
-    @property
-    def file(self: Self) -> io.TextIOWrapper:
-        if not hasattr(self, "_file"):
-            if self == "-":
-                self._file: io.TextIOWrapper = sys.stdout
-            else:
-                self._file: io.TextIOWrapper = open(self, "w")
-        return self._file
-
-    @classmethod
-    def to_text_output_path(cls: type(Self), path: Path) -> Self:
-        return cls(path)
-
-
 class Args(Tap):
     in_path: Path  # Input Quartus binary directory to search
     out_path: Path = "-"  # "Output JSON path (defaults to "-" for stdout)
@@ -151,7 +118,7 @@ class Args(Tap):
         self.add_argument("-o", "--out_path", type=TextOutputPath.to_text_output_path)
 
 
-def main():
+def main() -> None:
     args = Args().parse_args()
     real_main(args)
 
